17f04bc5364206cff52121ada0d21058c02efd61
Abdurakhim B <97093590+aBacoding@users.noreply.github.com>
2023-09-23 05:05:36 +0600

    Create MerkleTree.js. Task 3.5: Merkle Tree Integration

---
+++ MerkleTree.js
@@ -0,0 +1,34 @@
+// Import the crypto library for cryptographic functionalities
+const crypto = require('crypto')
+
+// 3.5 Merkle Tree Integration: Constructing a Merkle tree for transaction storage and verification.
+class MerkleTree {
+	constructor(transactions) {
+		this.transactions = transactions // Transactions to be included in the Merkle tree
+		this.tree = this.buildTree(transactions) // Building the tree with transactions
+	}
+
+	buildTree(transactions) {
+		if (transactions.length === 0) return null
+
+		if (transactions.length === 1) {
+			return this.hash(JSON.stringify(transactions[0])) // Return the hash of the single transaction
+		}
+
+		// Generate parent nodes by hashing pairs of child nodes (transactions)
+		const parentNodes = []
+		for (let i = 0; i < transactions.length; i += 2) {
+			const leftNode = JSON.stringify(transactions[i])
+			const rightNode = JSON.stringify(transactions[i + 1] || transactions[i])
+			const parentNodeHash = this.hash(
+				this.hash(leftNode) + this.hash(rightNode)
+			)
+			parentNodes.push(parentNodeHash)
+		}
+
+		// Recurse with the parent nodes as input, until reaching the root
+		return this.buildTree(parentNodes)
+	}
+}
+
+module.exports = MerkleTree // Exporting the MerkleTree class for use in other files
